7a907e69cde20d0f4e34c88cdc4b64d244757b9d
Federico Stagni <federico.stagni@cern.ch>
2016-01-05 16:42:07 +0100

    update

--- dirac.zsh
+++ dirac.zsh
@@ -1,1 +1,11 @@
 alias startDIRACRelease='cd $HOME/DIRAC_Releases && mkdir $(date +"20%y%m%d") && cd $_ && git clone git@github.com:DIRACGrid/DIRAC.git && cd DIRAC && git remote rename origin release'
+
+function startReleaseOf(){
+  # $1 should contain DIRAC or WebAppDIRAC
+  cd $HOME/DIRAC_Releases
+  mkdir $(date +"20%y%m%d") && cd $_ 
+  git clone git@github.com:DIRACGrid/$1.git 
+  cd $1
+  git remote rename origin release
+}
+
