f251b74cbbb61ddc9f9d57d807d9054bcb4e7775
Federico Stagni <federico.stagni@cern.ch>
2024-02-26 10:53:30 +0100

    Initial commit

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
+
+alias ll 'eza -al --icons'
+alias ghist 'history|grep'
+alias vi nvim
+alias ipy ipython
+alias py "python"
+alias pf "fzf --preview='less {}' --bind shift-up:preview-page-up,shift-down:preview-page-down"
+alias vif "fzf --print0 | xargs -0 -o nvim"
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/fstagni/mambaforge/bin/conda "shell.fish" hook $argv | source
+# <<< conda initialize <<<
